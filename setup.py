4979ca170e10f7a3703daed773c898f7acfa75eb
Ranjita <ranjitakavaloor7676@gmail.com>
2025-08-05 19:49:38 +0530

    Saving local changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='audio/Text to Sign Language Converter',
+    version='1.0.0',
+    description='Python mini project',
+    author='Voice2Sign Grp',
+    author_email='voice.sign.isl@gmail.com',
+    setup_requires=['nltk', 'joblib','click','regex','sqlparse','setuptools'],
+)
